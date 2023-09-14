75fc8bdc2cd9f7f035dfe08c096dd973cc6ddb54
debbie-herridge <debbie.herridge@icloud.com>
2023-09-14 15:03:56 +0000

    add html template for displaying turtles

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('turtles/', views.turtles, name='turtles'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+def turtles(request):
+    return render(request, 'turtles.html')
+
