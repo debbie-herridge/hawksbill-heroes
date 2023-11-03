0690b406f5225291d86955b9bc5dc608e4fdf04a
debbie-herridge <debbie.herridge@icloud.com>
2023-11-03 16:16:13 +0000

    style checkout basket

--- checkout/views.py
+++ checkout/views.py
@@ -14,9 +14,8 @@ def checkout(request):
         return redirect(reverse('merchandise'))
 
     order_form = OrderForm()
-    template = 'checkout.html'
     context = {
         'order_form':order_form,
     }
 
-    return render(request, template ,{})
+    return render(request, 'checkout.html' , context)
