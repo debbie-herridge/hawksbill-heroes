9dfe1f441715606acec41c4767aaa1e870df68fa
debbie-herridge <debbie.herridge@icloud.com>
2024-01-07 20:23:41 +0000

    update turtle details function

--- turtles/urls.py
+++ turtles/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('home/', views.turtles, name='turtles'),
     path('delete_turtle/<pk>', views.deleteTurtle, name='delete_turtle'),
+    path('update_turtle/<pk>', views.updateTurtle, name='update_turtle'),
     path('<turtle_id>/', views.turtleDetails, name='turtle_details'),
 ]
--- turtles/views.py
+++ turtles/views.py
@@ -33,7 +33,7 @@ def turtleDetails(request, turtle_id):
     context={
 	    'turtle':turtle,
     }
-    return render(request, 'turtle_details.html', context)
+    return render(request, 'turtle-details.html', context)
 
 def deleteTurtle(request, pk):
     """
@@ -49,3 +49,30 @@ def deleteTurtle(request, pk):
         'turtle':turtle,
     }
     return render(request, 'delete-turtle.html', context)
+
+def updateTurtle(request, pk):
+    """
+    Allow users to update their specific booking.
+    """
+    turtle = get_object_or_404(Turtle, pk=pk)
+    if request.method == 'POST':
+        form = TurtleForm(request.POST, instance=turtle)
+        if form.is_valid():
+            form.save()
+            return redirect('turtles')
+        else:
+            print('form not valid')
+            print(form.errors)
+    else:
+        form = TurtleForm(instance=turtle, initial={
+            "name":turtle.name,
+            "description":turtle.description,
+            "weight":turtle.weight,
+            "age":turtle.age,
+            "size":turtle.size,
+            "discovery_date":turtle.discovery_date,
+            })
+    context = {
+        'form':form,
+    }
+    return render(request, 'update-turtle.html', context)
