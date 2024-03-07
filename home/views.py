862ac3f2a84ca731a840508525bd861f8f0fea67
debbie-herridge <debbie.herridge@icloud.com>
2024-03-07 18:25:11 +0000

    add donation payment

--- home/views.py
+++ home/views.py
@@ -1,23 +1,41 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.conf import settings
 from django.http import JsonResponse
+
+import stripe
 
 def home(request):
     return render(request, 'index.html')
 
+@login_required
 def donate(request):
     return render(request, 'donate.html')
 
+@login_required
 def charge(request):
-    amount = 5
-    if request.method == 'POST':
-        print('Data:', request.POST)
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe.api_key = settings.STRIPE_SECRET_KEY
 
+    if request.method == 'POST':
+        
+        amount = int(request.POST['amount'])
+        customer = stripe.Customer.create(
+            source=request.POST['stripeToken']
+            )
+        charge = stripe.Charge.create(
+            customer=customer,
+            amount=amount*100,
+            currency='gbp',
+            description="Donation"
+            )
     return redirect(reverse('success', args=[amount]))
 
+@login_required
 def success_msg(request, args):
     amount = args
-    return render(request, 'success', {'amount':amount})
+    return render(request, 'donation_success.html', {'amount':amount})
 
 def handler404(request, exception):
     """ 
